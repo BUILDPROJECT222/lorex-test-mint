aa5bf61ba9dfafb3f406e6b331ff541fa1b1a81b
kuyol-project <153427860+BUILDPROJECT222@users.noreply.github.com>
2024-05-26 20:10:23 +0700

    Update settings.tsx

--- settings.tsx
+++ settings.tsx
@@ -1,14 +1,12 @@
 // Purpose: Contains texts that will be used in the UI when rendering the buttons
 // label has to be the exact same as in your candy machine config
 export const mintText = [
-  { label: "WL", mintText: "WL mint paying with SOL", buttonLabel: "Mint now!", header: "WL Mint" },
-  { label: "OG", mintText: "Mint Paying with ABC", buttonLabel: "Mint now!", header: "OG Mint" },
-  { label: "publi", mintText: "Mint Paying with DEF", buttonLabel: "Mint now!", header: "Public Mint" },
-  { label: "default", mintText: "default mintText", buttonLabel: "Mint now!", header: "Default"}
+  { label: "OGs", mintText: "WL mint paying with SOL", buttonLabel: "Mint now!", header: "WL Mint" },
+  { label: "Public", mintText: "Mint Paying with SOL", buttonLabel: "Mint now!", header: "Public Mint" },
 ];
 
 //header image in the ui. replace with your own
-export const image = "https://avatars.githubusercontent.com/u/93528482?v=4";
+export const image = "https://i.ibb.co.com/RvqxSRW/photo-2024-05-24-10-37-18.jpg";
 
 //website title
-export const headerText = "MarkSackerberg's mint UI";
+export const headerText = "Lorex Test Mint";
